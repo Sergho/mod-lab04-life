a26bcf2e5003b964745ed80b7a9255aa00f5434e
Sergho <sergche04@gmail.com>
2025-04-25 00:26:45 +0300

    feat: add cell tests

---
+++ Life.Tests/CellTests.cs
@@ -0,0 +1,54 @@
+﻿using cli_life;
+
+namespace Life.Tests;
+
+public class CellTests
+{
+    [Fact]
+    public void OneAliveNeighborTest()
+    {
+        Config config = Config.Parse("config.json");
+        Cell.Config = config.cell;
+        Cell cell = new Cell(0, 0) { IsAlive = true };
+        cell.Neighbors.AddRange(Enumerable.Repeat(new Cell(0, 0) { IsAlive = true }, 1));
+        cell.DetermineNextLiveState();
+        cell.Advance();
+        Assert.False(cell.IsAlive);
+    }
+
+    [Fact]
+    public void TwoAliveNeighborsTest()
+    {
+        Config config = Config.Parse("config.json");
+        Cell.Config = config.cell;
+        Cell cell = new Cell(0, 0) { IsAlive = true };
+        cell.Neighbors.AddRange(Enumerable.Repeat(new Cell(0, 0) { IsAlive = true }, 2));
+        cell.DetermineNextLiveState();
+        cell.Advance();
+        Assert.True(cell.IsAlive);
+    }
+
+    [Fact]
+    public void ThreeAliveNeighborsTest()
+    {
+        Config config = Config.Parse("config.json");
+        Cell.Config = config.cell;
+        Cell cell = new Cell(0, 0) { IsAlive = true };
+        cell.Neighbors.AddRange(Enumerable.Repeat(new Cell(0, 0) { IsAlive = true }, 3));
+        cell.DetermineNextLiveState();
+        cell.Advance();
+        Assert.True(cell.IsAlive);
+    }
+
+    [Fact]
+    public void FourAliveNeighborsTest()
+    {
+        Config config = Config.Parse("config.json");
+        Cell.Config = config.cell;
+        Cell cell = new Cell(0, 0) { IsAlive = true };
+        cell.Neighbors.AddRange(Enumerable.Repeat(new Cell(0, 0) { IsAlive = true }, 4));
+        cell.DetermineNextLiveState();
+        cell.Advance();
+        Assert.False(cell.IsAlive);
+    }
+}
